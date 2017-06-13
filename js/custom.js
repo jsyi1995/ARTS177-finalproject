b4b81900d1c57197abf4891c1e3b9c85f1f4adf9
ericcbonilla <ericcbonilla@gmail.com>
2017-06-13 15:41:39 -0700

    playing with nav bar

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 (function ($) {
-    
+
     // Add smooth scrolling to all links in navbar
     $(".navbar a,a.btn-appoint, .quick-info li a, .overlay-detail a").on('click', function(event) {
         event.preventDefault();
@@ -10,7 +10,7 @@
             window.location.hash = hash;
         });
     });
-       
+
     //jQuery to collapse the navbar on scroll
     $(window).scroll(function() {
         if ($(".navbar-default").offset().top > 50) {
@@ -19,5 +19,5 @@
             $(".navbar-fixed-top").removeClass("top-nav-collapse");
         }
     });
-    
+
 })(jQuery);
